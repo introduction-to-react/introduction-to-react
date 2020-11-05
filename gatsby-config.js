fe898d06e2b41f64d25ce343a334e7c54a9e7827
Michael König <koenigmichael2002@gmail.com>
2020-11-05 15:46:55 +0100

    Change name

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,12 +24,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Rocketseat Gatsby Themes`,
-        short_name: `RS Gatsby Themes`,
+        name: `Introduction to React`,
+        short_name: `Introduction to React`,
         start_url: `/`,
         background_color: `#ffffff`,
         display: `standalone`,
-        icon: `static/favicon.png`,
       },
     },
     `gatsby-plugin-sitemap`,
@@ -43,7 +42,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
-        siteUrl: `https://rocketdocs.netlify.com`,
+        siteUrl: `https://react.mkoe.io`,
       },
     },
     `gatsby-plugin-offline`,
